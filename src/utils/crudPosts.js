58ea95262ae7f4d6b32148ca89ff71c7ff55e585
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-22 14:56:21 +0200

    Update crudPosts.js

--- src/utils/crudPosts.js
+++ src/utils/crudPosts.js
@@ -1,11 +1,11 @@
 const URL = process.env.REACT_APP_BE_URL
-const POSTS_URL = `${URL}/posts`
-console.log("POSTS_URL",POSTS_URL)
+// const POSTS_URL = `${URL}/posts`
+// console.log("POSTS_URL",POSTS_URL)
 // GET POSTS
 export const getPosts = async () => {
   try {
-   console.log(POSTS_URL)
-    const response = await fetch(`${POSTS_URL}`)
+   console.log(URL)
+    const response = await fetch(`${URL}/posts`)
     console.log("response")
     if (response.ok) {
       const posts = await response.json();
